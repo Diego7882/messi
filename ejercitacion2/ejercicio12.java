68fc805b3b245768f8a7de084c1d6b5a45b5223f
Diego <diegorondal9@gmail.com>
2026-04-26 19:06:48 -0300

    Update ejercicio12.java

--- ejercitacion2/ejercicio12.java
+++ ejercitacion2/ejercicio12.java
@@ -1,5 +1,18 @@
 package ejericitacion2;
-
 public class ejercicio12 {
-
+   public static void main(String[] args) {
+       int[] numeros = {9, 40, 80, 10, 4, 6, 47, 22};
+       int mayor = numeros[0];
+       int menor = numeros[0];
+       for (int i = 0; i < 8; i++) {
+           if (numeros[i] > mayor) {
+               mayor = numeros[i];
+           }
+           if (numeros[i] < menor) {
+               menor = numeros[i];
+           }
+       }
+       System.out.println("El numero mayor es: " + mayor);
+       System.out.println("El numero menor es: " + menor);
+   }
 }
