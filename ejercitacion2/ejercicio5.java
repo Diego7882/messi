a4f61116027d11cbdde25538f1b37ef7d4be3b2d
Diego <diegorondal9@gmail.com>
2026-04-26 18:35:05 -0300

    Update ejercicio5.java

--- ejercitacion2/ejercicio5.java
+++ ejercitacion2/ejercicio5.java
@@ -1,5 +1,20 @@
 package ejericitacion2;
-
+import java.util.*;
 public class ejercicio5 {
-
+   public static void main(String[] args) {
+       Scanner teclado = new Scanner(System.in);
+       System.out.print("cuantos numeros desea ingresar: ");
+       int n = teclado.nextInt();
+       int[] numeros = new int[n];
+       int contador = 0;
+       for (int i = 0; i < n; i++) {
+           System.out.print("ingrese un numero: ");
+           numeros[i] = teclado.nextInt();
+           if (numeros[i] % 2 == 0) {
+               contador = contador + 1;
+           }
+       }
+       System.out.println("la cantidad de numeros pares son: " + contador);
+   }
 }
+
