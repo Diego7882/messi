61e602e37afb3458100ee265c32d05d30d0c2152
Diego <diegorondal9@gmail.com>
2026-04-26 18:34:34 -0300

    Update ejercicio4.java

--- ejercitacion2/ejercicio4.java
+++ ejercitacion2/ejercicio4.java
@@ -1,5 +1,19 @@
 package ejericitacion2;
-
+import java.util.*;
 public class ejercicio4 {
-
+   public static void main(String[] args) {
+       Scanner teclado = new Scanner(System.in);
+       System.out.print("cuantos numeros desea ingresar: ");
+       int n = teclado.nextInt();
+       int[] numeros = new int[n];
+       int suma = 0;
+       for (int i = 0; i < n; i++) {
+           System.out.print("ingrese un numero: ");
+           numeros[i] = teclado.nextInt();
+           suma = suma + numeros[i];
+       }
+       double promedio = (double) suma / n;
+       System.out.println("el promedio es de: " + promedio);
+   }
 }
+
