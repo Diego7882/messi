4a8fbaeef3a15db3a0046dadf357c528c363c87d
Diego <diegorondal9@gmail.com>
2026-04-26 18:36:10 -0300

    Update ejercicio8.java

--- ejercitacion2/ejercicio8.java
+++ ejercitacion2/ejercicio8.java
@@ -1,5 +1,26 @@
 package ejericitacion2;
-
+import java.util.*;
 public class ejercicio8 {
-
+   public static void main(String[] args) {
+       Scanner teclado = new Scanner(System.in);
+       String palabra = "muniain";
+       System.out.println("adivina la palabra letra x letra");
+       for (int i = 0; i < palabra.length(); i++) {
+           System.out.print("ingresa una letra: ");
+           char letra = teclado.next().charAt(0);
+           boolean encontrado = false;
+           for (int j = 0; j < palabra.length(); j++) {
+               if (letra == palabra.charAt(j)) {
+                   encontrado = true;
+               }
+           }
+           if (encontrado) {
+               System.out.println("correcto");
+           } else {
+               System.out.println("incorrecto");
+           }
+       }
+       System.out.println("fin del juego, gg");
+   }
 }
+
