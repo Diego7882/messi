c2f10efed0f73d8d0d26e5b0917a2da9eec2a71c
Diego <diegorondal9@gmail.com>
2026-04-26 21:39:20 -0300

    Add files via upload

---
+++ ejercitacion4/ejercicio2/Empleado.java
@@ -0,0 +1,12 @@
+package Ejercitacion4;
+
+public class Empleado {
+
+    protected int legajo;
+    protected double sueldoBase;
+
+    public Empleado(int legajo, double sueldoBase) {
+        this.legajo = legajo;
+        this.sueldoBase = sueldoBase;
+    }
+}
---
+++ ejercitacion4/ejercicio2/Operario.java
@@ -0,0 +1,11 @@
+package Ejercitacion4;
+
+public class Operario extends Empleado {
+
+    private double valorHoraExtra;
+
+    public Operario(int legajo, double sueldoBase, double valorHoraExtra) {
+        super(legajo, sueldoBase);
+        this.valorHoraExtra = valorHoraExtra;
+    }
+}
