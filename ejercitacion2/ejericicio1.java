22c142bdf55e46fa0a1cc0e9bd6862183788653e
Diego <diegorondal9@gmail.com>
2026-04-27 13:59:00 -0300

    Update ejericicio1.java

--- ejercitacion2/ejericicio1.java
+++ ejercitacion2/ejericicio1.java
@@ -3,12 +3,12 @@ import java.util.*;
 public class ejericicio1 {
    public static void main(String[] args) {
        Scanner teclado = new Scanner(System.in);
-       System.out.print("cantidad de numeros que desea ingresar: ");
+       System.out.print("¿cantidad de numeros que desea ingresar?: ");
        int n = teclado.nextInt();
        int[] numeros = new int[n];
        int suma = 0;
        for (int i = 0; i < n; i++) {
-           System.out.print("ingrese un numero: ");
+           System.out.print("ingrese un numero  ");
            numeros[i] = teclado.nextInt();
        }
        for (int i = 0; i < n; i++) {
